3f46c2481469f9b48dc165156f314bfe6d9e649d
Sam McCallum <sammccallum@138-38-193-159.eduroam.bath.ac.uk>
2025-03-07 10:18:40 +0000

    testing

--- benchmarks/reversible.py
+++ benchmarks/reversible.py
@@ -77,47 +77,50 @@ def measure_runtime(y0__args__term, solver, adjoint, stepsize_controller, dual_y
 
 
 if __name__ == "__main__":
-    n = 100
+    n = 10
     y0 = jnp.linspace(1, 10, num=n)
     key = jr.PRNGKey(10)
     f = VectorField(n, n, n, depth=2, key=key)
     terms = diffrax.ODETerm(f)
     args = jnp.linspace(0, 1, n)
-    base_solver = diffrax.Midpoint()
-    solver = diffrax.Reversible(base_solver)
+    base_solver = diffrax.LeapfrogMidpoint()
+    # solver = diffrax.Reversible(base_solver)
+    solver = base_solver
     t0 = 0
-    t1 = 5
+    t1 = 5.0
     dt0 = 0.01
     ts = jnp.linspace(t0, t1, num=500)
     stepsize_controller = diffrax.StepTo(ts=ts)
 
-    # saveat = diffrax.SaveAt(ts=ts)
+    saveat = diffrax.SaveAt(ts=ts)
     # saveat = diffrax.SaveAt(t1=True)
-    # adjoint = diffrax.ReversibleAdjoint()
-    # loss, grads = _loss(
-    #     (y0, args, terms), solver, saveat, adjoint, stepsize_controller, dual_y0=False
-    # )
-    # print(grads[2].vector_field.mlp.layers[0].weight)
-    # adjoint = diffrax.RecursiveCheckpointAdjoint()
-    # loss, grads = _loss(
-    #     (y0, args, terms),
-    #     base_solver,
-    #     saveat,
-    #     adjoint,
-    #     stepsize_controller,
-    #     dual_y0=False,
-    # )
-    # print("---")
-    # print(grads[2].vector_field.mlp.layers[0].weight)
-
-    print("Recursive")
-    adjoint = diffrax.RecursiveCheckpointAdjoint()
-    measure_runtime(
-        (y0, args, terms), base_solver, adjoint, stepsize_controller, dual_y0=False
-    )
-
-    print("Reversible")
     adjoint = diffrax.ReversibleAdjoint()
-    measure_runtime(
-        (y0, args, terms), solver, adjoint, stepsize_controller, dual_y0=False
+    loss, grads = _loss(
+        (y0, args, terms), solver, saveat, adjoint, stepsize_controller, dual_y0=False
     )
+    print(grads[0])
+    # print(grads[2].vector_field.mlp.layers[0].weight)
+    adjoint = diffrax.RecursiveCheckpointAdjoint()
+    loss, grads = _loss(
+        (y0, args, terms),
+        solver,
+        saveat,
+        adjoint,
+        stepsize_controller,
+        dual_y0=False,
+    )
+    print("---")
+    print(grads[0])
+    # print(grads[2].vector_field.mlp.layers[0].weight)
+
+    # print("Recursive")
+    # adjoint = diffrax.RecursiveCheckpointAdjoint()
+    # measure_runtime(
+    #     (y0, args, terms), base_solver, adjoint, stepsize_controller, dual_y0=False
+    # )
+
+    # print("Reversible")
+    # adjoint = diffrax.ReversibleAdjoint()
+    # measure_runtime(
+    #     (y0, args, terms), solver, adjoint, stepsize_controller, dual_y0=False
+    # )
